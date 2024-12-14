fce7b0135c465c85bc5f952c3320fcf95f76d06d
tavioribeiro <otavio123ribeiro@gmail.com>
2024-12-13 23:19:48 -0300

    🛠️ Ajustes 📙 Descrição: Ajuste do modal de filtros.

--- app/src/main/java/com/example/renerd/components/filters_dialog/adapters/FilterItemAdapter.kt
+++ app/src/main/java/com/example/renerd/components/filters_dialog/adapters/FilterItemAdapter.kt
@@ -1,15 +1,16 @@
 package com.example.renerd.components.filters_dialog.adapters
 
-import android.annotation.SuppressLint
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.renerd.R
-import com.example.renerd.core.utils.log
+import com.example.renerd.core.extentions.ContextManager
 import com.example.renerd.view_models.FiltersTabsItemModel
+import core.extensions.styleBackground
 import core.extensions.toTitleCase
 
 class FilterItemAdapter(
@@ -17,8 +18,11 @@ class FilterItemAdapter(
     private val onClick: (FiltersTabsItemModel) -> Unit
 ) : RecyclerView.Adapter<FilterItemAdapter.FilterItemViewHolder>() {
 
+    // Não é mais necessário, pois o status agora está no FiltersTabsItemModel
+    // var currentStatus: Boolean = true
+
     class FilterItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val checkBox: CheckBox = itemView.findViewById(R.id.checkBox)
+        //val main_container: LinearLayout = itemView.findViewById(R.id.main_container)
         val textView: TextView = itemView.findViewById(R.id.textView)
     }
 
@@ -31,11 +35,24 @@ class FilterItemAdapter(
         val item = filtersTabsListItemModel[position]
         holder.textView.text = item.label.toTitleCase()
 
-        holder.checkBox.setOnCheckedChangeListener(null)
-        holder.checkBox.isChecked = item.status
+        // Estiliza o fundo do itemLayout com base no status do item
+        if (item.status) {
+            holder.textView.styleBackground(
+                backgroundColor = ContextManager.getColorHex(6),
+                radius = 50f
+            )
+            holder.textView.setTextColor(Color.parseColor(ContextManager.getColorHex(1)))
+        } else {
+            holder.textView.styleBackground(
+                backgroundColor = ContextManager.getColorHex(2),
+                radius = 50f
+            )
+            holder.textView.setTextColor(Color.parseColor(ContextManager.getColorHex(5)))
+        }
 
-        holder.checkBox.setOnCheckedChangeListener { _, isChecked ->
-            item.status = isChecked
+        // Adiciona um OnClickListener para alternar o status
+        holder.textView.setOnClickListener {
+            item.status = !item.status
             onClick(item)
             notifyItemChanged(position)
         }
@@ -43,13 +60,5 @@ class FilterItemAdapter(
 
     override fun getItemCount(): Int {
         return filtersTabsListItemModel.size
-    }
-
-    @SuppressLint("NotifyDataSetChanged")
-    fun selectAll(select: Boolean) {
-        for (item in filtersTabsListItemModel) {
-            item.status = select
-        }
-        notifyDataSetChanged()
     }
 }
