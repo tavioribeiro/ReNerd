5370655da10993d67866be08cb2e05161f0ebc14
tavioribeiro <otavio123ribeiro@gmail.com>
2025-03-09 00:24:47 -0300

    - Merge

--- app/src/main/java/com/example/renerd/components/filters_dialog/FiltersDialog.kt
+++ app/src/main/java/com/example/renerd/components/filters_dialog/FiltersDialog.kt
@@ -60,7 +60,6 @@ class FiltersDialog(
 
 
         binding.saveButtom.setOnClickListener(){
-            //log(mixedFiltersItens)
             onSave(mixedFiltersItens)
             this.dismiss()
         }
